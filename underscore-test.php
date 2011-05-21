847a798b5705270f57e4607db11e080b59c318b5
Brian Haveri <brian.haveri@gmail.com>
2011-05-21 11:47:22 -0600

    Initial import with a few methods

---
+++ underscore-test.php
@@ -0,0 +1,73 @@
+<?php
+
+include('underscore.php');
+
+class UnderscoreTest extends PHPUnit_Framework_TestCase {
+  
+  public function testMap() {
+    $iterator = function($val, $key=null) { return $val * 3; };
+    $collection = array('one'=>1, 'two'=>2, 'three'=>3);
+    $expected = array(3, 6, 9);
+    
+    // Array: keyed
+    $this->assertEquals($expected, _::map($collection, $iterator));
+    
+    // Array: zero indexed
+    $zero_indexed = array_values($collection);
+    $this->assertEquals($expected, _::map($zero_indexed, $iterator));
+    
+    // Object
+    $collection_obj = (object) $collection;
+    $this->assertEquals($expected, _::map($collection_obj, $iterator));
+  }
+  
+  public function testPluck() {
+    // Array
+    $stooges = array(
+      array('name'=>'moe',   'age'=> 40),
+      array('name'=>'larry', 'age'=> 50),
+      array('name'=>'curly', 'age'=> 60)
+    );
+    $tests = array(
+      'name'=> array('moe', 'larry', 'curly'),
+      'age' => array(40, 50, 60)
+    );
+    foreach($tests as $key=>$expected) {
+      $this->assertEquals($expected, _::pluck($stooges, $key));
+    }
+    
+    // Object
+    $stooges_obj = new StdClass;
+    foreach($stooges as $stooge) {
+      $name = $stooge['name'];
+      $stooges_obj->$name = (object) $stooge;
+    }
+    foreach($tests as $key=>$expected) {
+      $this->assertEquals($expected, _::pluck($stooges_obj, $key));
+    }
+  }
+  
+  public function testIncludes() {
+    $collection = array(true, false, 0, 1, -1, 'foo', array(), array('meh'));
+    $tests = array(
+      // val, expected
+      array(true, true),
+      array(false, true),
+      array(0, true),
+      array(1, true),
+      array(-1, true),
+      array('foo', true),
+      array(array(), true),
+      array(array('meh'), true),
+      array('true', false),
+      array('0', false),
+      array('1', false),
+      array('-1', false),
+      array('bar', false),
+      array('Foo', false)
+    );
+    foreach($tests as $test) {
+      $this->assertEquals($test[1], _::includes($collection, $test[0]));
+    }
+  }
+}
---
+++ underscore.php
@@ -0,0 +1,28 @@
+<?php
+
+class _ {
+  
+  public static function map($collection, $iterator) {
+    $collection = (array) $collection;
+    $return = array();
+    foreach($collection as $k=>$v) {
+      $return[] = call_user_func($iterator, $v, $k, $collection);
+    }
+    return $return;
+  }
+  
+  public static function pluck($collection, $key) {
+    $return = array();
+    foreach($collection as $item) {
+      foreach($item as $k=>$v) {
+        if($k === $key) $return[] = $v;
+      }
+    }
+    return $return;
+  }
+  
+  public function includes($collection, $val) {
+    $collection = (array) $collection;
+    return is_int(array_search($val, $collection, true));
+  }
+}
