94f0eafc5c836a0ebe18c6100f8c1729a9b56e5c
Brian Haveri <brian.haveri@gmail.com>
2011-05-31 00:52:25 -0600

    Test - Add ChainingTest

---
+++ test/ChainingTest.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once(__DIR__ . '/../underscore.php');
+
+class UnderscoreChainingTest extends PHPUnit_Framework_TestCase {
+  
+  public function testMapFlattenReduce() {
+    // from js
+    $lyrics = array(
+      "I'm a lumberjack and I'm okay",
+      "I sleep all night and I work all day",
+      "He's a lumberjack and he's okay",
+      "He sleeps all night and he works all day"
+    );
+    $counts = _($lyrics)->chain()
+      ->map(function($line) { return str_split($line); })
+      ->flatten()
+      ->reduce(function($hash, $l) {
+        if(!is_array($hash)) $hash = array();
+        $hash[$l] = array_key_exists($l, $hash) ? $hash[$l] : 0;
+        $hash[$l]++;
+        return $hash;
+      })
+      ->value();
+    $this->assertTrue($counts['a'] === 16 && $counts['e'] === 10, 'counted all the letters in the song');
+  }
+  
+  public function testSelectRejectSortBy() {
+    $numbers = array(1,2,3,4,5,6,7,8,9,10);
+    $numbers = _($numbers)->chain()->select(function($n) {
+      return $n % 2 === 0;
+    })->reject(function($n) {
+      return $n % 4 === 0;
+    })->sortBy(function($n) {
+      return -$n;
+    })->value();
+    $this->assertEquals(array(10, 6, 2), $numbers, 'filtered and reversed the numbers');
+  }
+}
