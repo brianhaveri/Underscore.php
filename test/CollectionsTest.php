a06734f3468e91b2e255f2d3c526394086993c01
Brian Haveri <brian.haveri@gmail.com>
2011-05-31 18:33:24 -0600

    Added OO test for map()

--- test/CollectionsTest.php
+++ test/CollectionsTest.php
@@ -60,20 +60,13 @@ class UnderscoreCollectionsTest extends PHPUnit_Framework_TestCase {
     $ifnull = _::map(null, function() {});
     $this->assertTrue(is_array($ifnull) && count($ifnull) === 0, 'handles a null property');
     
-    // @todo
-    /*
-    var tripled = _.map([1, 2, 3], function(num){ return num * this.multiplier; }, {multiplier : 3});
-    equals(tripled.join(', '), '3, 6, 9', 'tripled numbers with context');
+    $multiplier = 3;
+    $func = function($num) use ($multiplier) { return $num * $multiplier; };
+    $tripled = _::map(array(1,2,3), $func);
+    $this->assertEquals(array(3,6,9), $tripled);
     
-    var doubled = _([1, 2, 3]).map(function(num){ return num * 2; });
-    equals(doubled.join(', '), '2, 4, 6', 'OO-style doubled numbers');
-    
-    var ids = _.map(document.body.childNodes, function(n){ return n.id; });
-    ok(_.include(ids, 'qunit-header'), 'can use collection methods on NodeLists');
-    
-    var ids = _.map(document.images, function(n){ return n.id; });
-    ok(ids[0] == 'chart_image', 'can use collection methods on HTMLCollections');
-    */
+    $doubled = _(array(1,2,3))->map(function($num) { return $num * 2; });
+    $this->assertEquals(array(2,4,6), $doubled, 'OO-style doubled numbers');
   }
   
   public function testDetect() {
