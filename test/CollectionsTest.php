608b945f0d1f80b382c7e643cf4c84af145f6c41
Brian Haveri <brian.haveri@gmail.com>
2011-05-31 18:47:03 -0600

    Test - Added OO test for any()

--- test/CollectionsTest.php
+++ test/CollectionsTest.php
@@ -149,6 +149,8 @@ class UnderscoreCollectionsTest extends PHPUnit_Framework_TestCase {
     $this->assertTrue(_::any(array(1)));
     $this->assertTrue(_::any(array('1')));
     $this->assertTrue(_::any(array(1,2,3,4)));
+    $this->assertTrue(_(array(1,2,3,4))->any(), 'works with OO-style calls');
+    $this->assertFalse(_(array(1,11,29))->any(function($num) { return $num % 2 === 0; }));
     
     // @todo
     /*
