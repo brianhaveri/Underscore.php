247face9fdb287f5d58ac59e167c01f6cb9d2259
Blaine Schmeisser <BlaineSch@gmail.com>
2012-06-29 01:14:04 -0500

    Changed _chained to public

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
 include_once(__DIR__ . '/../underscore.php');
+
+$interceptor = function($obj) { return $obj * 2; };
+$a = new __();
+echo $a->chain(array(1, 2, 3))->max()
+                         ->tap($interceptor)
+                         ->value(); // 6
